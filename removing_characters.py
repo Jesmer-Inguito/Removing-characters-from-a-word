870bc200cdc6fb9ff563945a6d5ca9aa057c059f
unknown <you@example.com>
2024-01-23 23:00:32 +0800

    created a variable with string slicer inside

--- removing_characters.py
+++ removing_characters.py
@@ -5,6 +5,7 @@ word = input("Enter your chosen word: ")
 
 # Print the chosen word
 print("The word you've chosen is", word)
-# Cut the chosen word
 
+# Cut the chosen word "word[n:]", can change the n to different numbers
+edited = word[3:]
 # Print results
